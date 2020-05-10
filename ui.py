5cabbf55a8ed9c0cfa0ae3d28f291e3615a92ff1
itookyourboo <Wignorbo@yandex.ru>
2020-05-10 16:12:05 +0300

    Fixed bug with empty buttons Got rid of hardcode

--- ui.py
+++ ui.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 from states import State
+from strings import *
 
 
 class UI:
@@ -15,14 +16,19 @@ class UI:
         if 'buttons' in res['response']:
             user['last_btns'] = deepcopy(res['response']['buttons'])
         else:
-            res['response']['buttons'] = deepcopy(user['last_btns'])
+            res['response']['buttons'] = deepcopy(user.get('last_btns', []))
 
         if user['state'] == State.PLAY:
-            txt = 'Подскажи' if not user['used_hint'] else 'Помощь'
-            if all(x['title'] not in ('Подскажи', 'Помощь') for x in res['response']['buttons']):
+            txt = get(HINT_BTN) if not user['used_hint'] else get(HELP_BTN)
+            if all(x['title'] not in list(HINT_BTN) + list(HELP_BTN) for x in res['response']['buttons']):
                 res['response']['buttons'].append(UI.button(txt))
+
+            change_btn = UI.button(get(CHANGE_BTN))
+            if change_btn not in res['response'].get('buttons', []):
+                res['response'].get('buttons', []).append(change_btn)
+
         elif user['state'] == State.HELLO:
-            for button in ['Помощь', 'Что ты умеешь?']:
+            for button in [get(HELP_BTN), get(WHAT_YOU_CAN_BTN)]:
                 button_dict = UI.button(button)
                 if button_dict not in res['response']['buttons']:
                     res['response']['buttons'].append(button_dict)
